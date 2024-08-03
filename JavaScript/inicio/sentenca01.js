0f34c3b7e6dc76f901aae4d4e257c12fb656a7ab
onmagxp <matheusaguileraeu@hotmail.com>
2024-08-03 18:34:38 +0200

    JavaScript
    Material de Javascript

---
+++ JavaScript/inicio/sentenca01.js
@@ -0,0 +1,5 @@
+console.log('Teste!');
+console.log('Bom dia!');
+console.log('Boa Tarde!');
+console.log('Boa Noite!');
+// cada linha acima é uma sentença. Não por estarem separados por linha, mas, sim porque cada uma é um comando diferente dado ao computador para ser executado.
